dc20534660e97f0b7fe06616bb6af1801041e83c
Andres Malagon <andresmalagon@Andress-MacBook-Pro.local>
2022-02-09 13:06:30 -0500

    contact form

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
 import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
     const { name, description } = currentCategory;
@@ -8,7 +8,7 @@ function Gallery({ currentCategory }) {
         <section>
             <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
             <p>{description}</p>
-            <PhotoList />
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 }
